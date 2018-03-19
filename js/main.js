332a2b7d3a4494da2b6477b022468b6b2fb1545b
Lyndsay Fearnall <lyndsayfearnall@lyndsays-mbp-2.fc.ca>
2018-03-19 13:40:19 -0400

    adding in data

--- js/main.js
+++ js/main.js
@@ -13,17 +13,34 @@ const vm = new Vue({
 
     anchorOff : true,
 
-    deliciousFruit : [
-      { name: "apples", flavour: "tangy!"},
-      { name: "peaches", flavour: "sweet"},
-      { name: "grapes", flavour: "squishy" }
-    ]
+    moviedata : [
+      { name: "Star Wars the Force Awakens", thumb : "forceawakens.jpg", vidsource : "forceawakens.mp4", description: "Yet another star wars movie"},
+      { name: "The Avengers", thumb : "avengers.jpg", vidsource : "avengers.mp4", description: "Something about superheroes... wah wah..."},
+      { name: "Stranger Things", thumb : "strangerthings.jpg", vidsource : "strangerthings.jpg", description : "The best show. " }
+    ],
+
+    moviename : "movie name goes here",
+    moviedescription : "movie description goes here",
+    moviesource : "null"
   },
 
 //"this" method is different in vue, points to vue itself
   methods : {
-    logFruit(e){
+    logMovie(e){
+      e.preventDefault();
+
       console.log(e.currentTarget);
+
+      //find the href (name of video) and use it to filter the collection
+      //stripping filename out, removing everything else
+      datakey = e.currentTarget.href.substring(e.currentTarget.href.lastIndexOf('/') + 1);
+
+      //filter the collection and us the video name as a ref
+      currentData = this.moviedata.filter(video => video.vidsource === datakey);
+
+      this.moviename = currentData[0].name;
+      this.moviedescription = currentData[0].description;
+      this.moviesource = datakey;
     }
   }
 });
